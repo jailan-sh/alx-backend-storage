758f6c01198f248409fcd806fbfe105dd402d0b0
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-22 00:40:37 +0200

    2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,3 @@
 -- ranking band origin
-SELECT origin FROM metal_bands
-SUM(fans) AS nb_fans GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+ GROUP BY origin ORDER BY nb_fans DESC;
