2992ab18f50435001df1e970329dd5810b5dd6b5
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-22 00:54:11 +0200

    edit

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- select bands with Glam Rock as their main style, ranked by their longevity
-SELECT DISTINCT band_name, IF NULL(split, 2020) - formed AS lifespan
+SELECT DISTINCT band_name, IFNULL(split, 2020) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
