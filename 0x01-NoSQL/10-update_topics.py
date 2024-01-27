426747feca96595f6c1167ef23f615ea200ca1ed
jailan-sh <jilan.elsherbiny@gmail.com>
2024-01-27 03:02:21 +0200

    10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" update document"""
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update"""
+    mongo_collection.update_many({'name':name}, {$set: {'topics':topics}})
